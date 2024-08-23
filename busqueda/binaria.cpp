6c67b45b5f95446ae65a25840634d3a895f9f27a
juanF9910 <juan.zapata97@udea.edu.co>
2024-08-23 09:08:19 -0500

    agst-23

--- busqueda/binaria.cpp
+++ busqueda/binaria.cpp
@@ -22,6 +22,20 @@ bool busquedaBinaria(T* arreglo, int tamano, T valor) {
     return false;
 }
 
+/*
+la ventaja de los algoritmos recursivos es que son más fáciles de entender, 
+su implementación es más sencilla que la de los algoritmos iterativos
+la desventaja es que pueden ser menos eficientes que los algoritmos iterativos, 
+ya que cada llamada a la función recursiva implica un cambio de contexto
+y la creación de una nueva pila de llamadas
+en general, los algoritmos recursivos son más lentos que los iterativos, 
+pero en algunos casos, como en la búsqueda binaria, 
+la recursividad puede ser más eficiente
+la recursividad es una técnica muy útil para resolver problemas que se 
+pueden dividir en subproblemas más pequeños
+y que se pueden resolver de forma similar al problema original 
+*/
+
 template <class T>
 bool busqueda_recursive(T* arreglo, T valor, int inicio, int fin){
 
@@ -34,6 +48,7 @@ bool busqueda_recursive(T* arreglo, T valor, int inicio, int fin){
     if (arreglo[medio] == valor) { //caso base o condición de parada
         return true;
     }
+    
     if (arreglo[medio] < valor) {
         return busqueda_recursive(arreglo, valor, medio + 1, fin); //modifico el inicio para tomar la mitad de la derecha
     } else {
--- graficado/grafica.py
+++ graficado/grafica.py
@@ -5,3 +5,17 @@ if __name__ == '__main__':
     fig = figure() #creamos un objeto de la clase figure
     fig.line([1, 2, 3, 4, 5], [5, 4, 3, 2, 1], line_width=2) #dibujamos una línea
     show(fig)
+
+
+#el comando para crear un virtual enviroment en python es:
+#python -m venv nombre_del_entorno
+#el comando para activar el entorno es:
+#source nombre_del_entorno\bin\activate
+#el comando para instalar las dependencias es:
+#pip install -r requirements.txt
+#el comando para instalar una dependencia es:
+#pip install nombre_de_la_dependencia
+#el comando para desactivar el entorno es:
+#deactivate
+#el comando para eliminar el entorno es:
+#rm -rf nombre_del_entorno
